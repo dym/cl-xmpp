10217f987eadb3baa56eef645da3dd72b45ed9e9
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2012-12-24 16:52:28 +0200

    Added simple xmpp client as an example

---
+++ examples/cl-xmpp-client.lisp
@@ -0,0 +1,28 @@
+(in-package :xmpp-client)
+
+(setf *random-state* (make-random-state t))
+
+(defvar *connection* NIL)
+
+(defun auth (username password)
+  (let* ((random-part (random 10000))
+         (resource (concatenate 'string
+                                "cl-xmpp-client-"
+                                (write-to-string random-part))))
+    (xmpp:auth *connection*
+               username password
+               resource
+               :mechanism :sasl-digest-md5)
+    resource))
+
+(defun start-session (resource)
+    (xmpp:bind *connection* resource)
+    (xmpp:session *connection*)
+    (xmpp:get-roster *connection*)
+    (xmpp:presence *connection* :priority 5))
+
+(defun main (username password)
+  (defparameter *connection* (xmpp:connect :hostname "localhost"))
+  (let ((resource (auth username password)))
+    (start-session resource))
+)
---
+++ examples/packages.lisp
@@ -0,0 +1,7 @@
+(in-package :cl-user)
+
+(defpackage :cl-xmpp-client
+  (:use :cl)
+  (:nicknames :xmpp-client)
+  (:export
+   :main))

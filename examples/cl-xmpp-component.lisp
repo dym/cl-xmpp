271dba928ce1cc724316dbc0cafabab11e6ecbd8
Dmitriy Budashny <dmitriy.budashny@gmail.com>
2012-12-30 19:50:07 +0200

    Made example component to be able to connect

--- examples/cl-xmpp-component.lisp
+++ examples/cl-xmpp-component.lisp
@@ -3,3 +3,23 @@
 (setf *random-state* (make-random-state t))
 
 (defvar *connection* NIL)
+
+(defun auth (password)
+  (xmpp:auth *connection*
+             nil password
+             nil))
+
+(defun start-session (resource)
+    (xmpp:bind *connection* resource)
+    (xmpp:session *connection*)
+    (xmpp:get-roster *connection*)
+    (xmpp:presence *connection* :priority 5))
+
+(defun main (component password &key
+                                  (hostname "localhost")
+                                  (port 5400))
+  (defparameter *connection*
+    (xmpp:connect-component :hostname hostname
+                            :port port
+                            :server-jid component))
+  (auth password))
